96d3b3fb3538ceb7e7311db915a35be3117932c8
JoonHo135 <junohb3@naver.com>
2020-08-03 10:42:21 +0900

    시리얼 통신코드 작성완료

--- Harbor_Android/harbor_app/app/src/main/java/com/example/harbor_app/MainDrive.java
+++ Harbor_Android/harbor_app/app/src/main/java/com/example/harbor_app/MainDrive.java
@@ -3,8 +3,43 @@ package com.example.harbor_app;
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import com.github.nkzawa.emitter.Emitter;
+import com.github.nkzawa.socketio.client.IO;
+import com.github.nkzawa.socketio.client.Socket;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.net.URISyntaxException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.hardware.usb.UsbDeviceConnection;
 import android.hardware.usb.UsbManager;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -13,6 +48,7 @@ import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 import com.github.nkzawa.emitter.Emitter;
 import com.github.nkzawa.socketio.client.IO;
@@ -26,13 +62,11 @@ import org.json.JSONObject;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.List;
 import java.util.Timer;
 import java.util.TimerTask;
 
-public class MainDrive extends AppCompatActivity {
+public class MainDrive extends AppCompatActivity implements LocationListener {
     private Socket socket;
     TextView spdText;
     TextView angText;
@@ -41,6 +75,12 @@ public class MainDrive extends AppCompatActivity {
     UsbSerialPort port;
     int numBytesRead;
     String substr, Command;
+    LocationManager locationManager;
+    String gpsX;
+    String gpsY;
+    List<String> listProviders;
+    private TextView gpsLatitude, gpsLongitude;
+    Location lastKnownLocation;
 
     @SuppressLint("SimpleDateFormat")
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -48,8 +88,27 @@ public class MainDrive extends AppCompatActivity {
         setContentView(R.layout.drive_main);
         stpBtn = (Button) findViewById(R.id.stopBtn);
         //InitArduino();
+        //gps권한체크
+        int permissionCheck = ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION);
+        if (permissionCheck == PackageManager.PERMISSION_GRANTED) {
+            Toast.makeText(getApplicationContext(), "GPS 권한 있음", Toast.LENGTH_SHORT).show();
+        }
+        //gps수신
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        lastKnownLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        if (lastKnownLocation != null) {
+            double lng = lastKnownLocation.getLongitude();
+            double lat = lastKnownLocation.getLatitude();
+            gpsX = String.valueOf(lat);
+            gpsY = String.valueOf(lng);
+        }
+        listProviders = locationManager.getAllProviders();
+        if (listProviders.get(0).equals(LocationManager.GPS_PROVIDER)) {
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 100, this);
+        }
+        //소켓통신
         try {
-            socket = IO.socket("http://10.210.24.17:8080/");
+            socket = IO.socket("http://14.32.187.245:8080/");
             socket.connect();
             //////////////////////////////////////////////////
             socket.on(Socket.EVENT_CONNECT, onConnect);
@@ -100,6 +159,21 @@ public class MainDrive extends AppCompatActivity {
         }
     };
 
+    void sendDrone() {
+        JSONObject droneInfo = new JSONObject();
+        try{
+            droneInfo.put("gpsX",gpsX);
+            droneInfo.put("gpsY",gpsY);
+
+        }
+        catch(JSONException e)
+        {
+            e.printStackTrace();
+        }
+        socket.emit("drone data stream", droneInfo);
+    }
+
+
     public static byte[] binaryStringToByteArray(String s) {
         int count = s.length() / 8;
         byte[] b = new byte[count];
@@ -111,7 +185,7 @@ public class MainDrive extends AppCompatActivity {
     }
 
     public static byte binaryStringToByte(String s) {
-        byte ret = 0, total = 0;
+        byte ret, total = 0;
         for (int i = 0; i < 8; ++i) {
             ret = (s.charAt(7 - i) == '1') ? (byte) (1 << i) : 0;
             total = (byte) (ret | total);
@@ -131,8 +205,8 @@ public class MainDrive extends AppCompatActivity {
 
     public String byteArrayToBinaryString(byte[] b) {
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < b.length; ++i) {
-            sb.append(byteToBinaryString(b[i]));
+        for (byte value : b) {
+            sb.append(byteToBinaryString(value));
         }
         return sb.toString();
     }
@@ -255,4 +329,70 @@ public class MainDrive extends AppCompatActivity {
 
     }
 
+    //gps//
+    protected void onStart() {
+        super.onStart();
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            //권한이 없을 경우 최초 권한 요청 또는 사용자에 의한 재요청 확인
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, android.Manifest.permission.ACCESS_FINE_LOCATION) &&
+                    ActivityCompat.shouldShowRequestPermissionRationale(this, android.Manifest.permission.ACCESS_COARSE_LOCATION)) {
+                // 권한 재요청
+                ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION, android.Manifest.permission.ACCESS_COARSE_LOCATION}, 100);
+            } else {
+                ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION, android.Manifest.permission.ACCESS_COARSE_LOCATION}, 100);
+            }
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        locationManager.removeUpdates(this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
+    }
+
+    // @SupresspLint("SetTextI18n")
+    @SuppressLint("SetTextI18n")
+    public void onLocationChanged(Location location) {
+
+        double latitude;
+        double longitude;
+
+        if (location.getProvider().equals(LocationManager.GPS_PROVIDER)) {
+            latitude = location.getLatitude();
+            longitude = location.getLongitude();
+            gpsLatitude.setText(": " + latitude);
+            gpsLongitude.setText((": " + longitude));
+            Log.d("GPS", latitude + '/' + Double.toString(longitude));
+            gpsX = String.valueOf(latitude);
+            gpsY = String.valueOf(longitude);
+        }
+    }
+
+    public void onProviderEnabled(String provider) {
+        /*if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+        }
+        //locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 100, this);
+        //locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, this);
+        //locationManager.requestLocationUpdates(LocationManager.PASSIVE_PROVIDER, 0, 0, this);*/
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }//gps//
 }
