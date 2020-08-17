90e1ad4efd822b9c483ceebce1e795bac798b356
Unknown <fulmeach787@gmail.com>
2020-08-17 17:26:29 +0900

    관제 시연 애니메이션 , 충돌감지 완성

--- Harbor_Server/public/js/initMap.js
+++ Harbor_Server/public/js/initMap.js
@@ -2,7 +2,7 @@ var numDeltas = 1000;
 var map;
 var infowindow = null;
 
-var circles = [
+var start = [
   {lat: 35.514020, lng: 129.391979},
   {lat: 35.469623, lng: 129.393169}
 ];
@@ -14,6 +14,9 @@ var destination =[
 
 var deltaLat = [];
 var deltaLng = [];
+
+var cirRadius = [];
+var circles = [];
 
 function initMap() {
 
@@ -28,19 +31,14 @@ function initMap() {
   map = new google.maps.Map(
     document.getElementById('map'), {zoom: 13, center: ulsan});
 
-    for (var i=0; i<circles.length; i++){
-      console.log(circles[i], destination[i]);
-    }
-
     google.maps.event.addListenerOnce(map, 'tilesloaded', function(){
-        for (var i=0; i<circles.length; i++){
-          createCircle(circles[i], destination[i], map, i);
+        for (var i=0; i<start.length; i++){
+          createCircle(start[i], destination[i], map, i);
         }
       });
 }
 
 google.maps.event.addDomListener(window, 'load', initMap);
-
 
 function createCircle(start, end, map, content) {
     var circleOption = {
@@ -55,17 +53,33 @@ function createCircle(start, end, map, content) {
     }
     var circle = new google.maps.Circle(circleOption);
 
-  /*
-    var endPoints = {
-      start1: {lat: 35.497021, lng: 129.401589 },
-      start2: {lat: 35.497021, lng: 129.381589 }  
-    }
-  */
     var startPos = start;
     var endPos = end;
-  
+    
+    cirRadius[content] = circle.getRadius();
     deltaLat[Number(content)] = (endPos.lat - startPos.lat)/numDeltas;
     deltaLng[Number(content)] = (endPos.lng - startPos.lng)/numDeltas;
+
+    // 원의 center 에서 반지름 값을 더한 곳의 lat, lng 필요 
+    //var recLatLng1 = new google.maps.LatLng(startPos.lat + 0.01, startPos.lng + 0.01);
+    //var recLatLng2 = new google.maps.LatLng(startPos.lat - 0.01, startPos.lng - 0.01);
+    //console.log('이건데' + recLatLng1.lat() + ' ' + recLatLng1.lng()+ ' ' + recLatLng2.lat()+ ' ' + recLatLng2.lng());
+
+
+    var point = new google.maps.LatLng(startPos.lat, startPos.lng);
+    bounds = computingOffset(point);
+
+    var recOption = {
+      strokeColor: "#FF0000",
+      strokeOpacity: 0.8,
+      strokeWeight: 2,
+      fillColor: "#FF0000",
+      fillOpacity: 0.35,
+      map: map,
+      bounds: bounds
+    }
+
+    var rectangle = new google.maps.Rectangle(recOption);
 
     var circleTimer = setInterval (function() {
       startPos.lat += deltaLat[Number(content)];
@@ -74,9 +88,50 @@ function createCircle(start, end, map, content) {
       var latlng = new google.maps.LatLng(startPos.lat,startPos.lng);
       circleOption.center = latlng; 
       circle.setOptions(circleOption);
+
+      point = latlng;
+      recOption.bounds = computingOffset(point, content);
+      rectangle.setOptions(recOption);
+
+      circles[content] = latlng;
+      collisionCheck(content);
+      //rectangle.setOptions(recOption);
     }, 20);
+
 }
 
-function collisionCheck (){
-  console.log('')
+function computingOffset(center, content){
+  var spherical = google.maps.geometry.spherical; 
+  var areaRadi = cirRadius[content] * 0.8;
+  
+  var areaRadi2 = cirRadius[content] * 0.5;
+    var north = spherical.computeOffset(center, areaRadi, 0); 
+    var west  = spherical.computeOffset(center, areaRadi2, -90); 
+    var south = spherical.computeOffset(center, areaRadi, 180); 
+    var east  = spherical.computeOffset(center, areaRadi2, 90);
+
+    var bounds = {
+      north: north.lat(),
+      south: south.lat(),
+      east: east.lng(),
+      west: west.lng() 
+    }
+
+    return bounds;
 }
+
+function collisionCheck(circleNum){
+  var distance = google.maps.geometry.spherical.computeDistanceBetween (circles[0], circles[1]);
+
+  var totalRadi = cirRadius[0] + cirRadius[1];
+  if ( distance <= totalRadi){
+    $("#danger").text('매우 위험');
+  } else if ( distance <= totalRadi * 1.8){
+    $("#danger").text('위험');
+  } else {
+      $("#danger").text('보통');
+  }
+}
+
+
+
