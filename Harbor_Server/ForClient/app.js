bb6e58551c5233451781dfbc4903affeb5073b72
rlasy0126 <rlasy0126@naver.com>
2020-11-14 00:44:21 +0900

    컨트롤러용 서버2

--- Harbor_Server/ForClient/app.js
+++ Harbor_Server/ForClient/app.js
@@ -1,16 +1,30 @@
-const SerialPort = require('serialport');
-const io = require('socket.io');
-/*
-// Promise approach
-SerialPort.list().then(ports => {
-  ports.forEach(function(port) {
-    console.log(port.path);
-    console.log(port.pnpId);
-    console.log(port.manufacturer);
-  });
+var socket = require('socket.io-client')('ws://ksyksy12.iptime.org:33337');
+//var socket = notSocket.socket;
+//var socket = io.connect("http://ksyksy12.iptime.org:33337"); 
+
+//var socket = io.connect('localhost:8000'); 
+socket.on('connect', function(){
+  console.log("connected");
 });
-*/
-const sp = new SerialPort("COM5", { baudRate:9600, autoOpen:false });
+socket.on('disconnect', function(){
+  console.log("disconnected");
+});
+
+socket.on('news', function (data) { 
+  console.log(data.serverData);
+}); 
+
+// userid ??? db or websocket.id?
+socket.emit('client connected', 
+{ clientData : '드론 컨트롤러 접속', clientType : 'ctw', userid : 'test'}); 
+
+socket.emit('control stream', { gear: 1 , angle : 1, controlTime: 1 });
+
+const SerialPort = require('serialport');
+const Readline = require('@serialport/parser-readline');
+const sp = new SerialPort("COM5", { baudRate:9600, autoOpen:false});
+
+const parser = sp.pipe(new Readline({ delimiter: '\r\n' }))
 
 function timeDelay(timeout) {
 	return new Promise((resolve) => {
@@ -19,30 +33,18 @@ function timeDelay(timeout) {
 }
 
 sp.open(function() {
-  timeDelay(1000);
+  timeDelay(2000);
   
-	sp.on("error", function(error) {
+	parser.on("error", function(error) {
 		console.log("Error : ", error.message);
   });
 
-  port.on('data',function (data) {
+  parser.on('data',function (data) {
+    var str = data;
     console.log('Read and Send Data : ' + data);
-    //var controlTimeVal = new Date();
-    //var split = string.split(',');
-    //socket.emit('control stream', { gear: split[0] , angle : split[0], controlTime: controlTimeVal });
+    var controlTimeVal = new Date();
+    var split = str.split(" ");
+    socket.emit('control stream', { gear: split[0] , angle : split[1], controlTime: controlTimeVal });
   });
 });
-
-/*
-var socket = io.connect(porcess.argv[0]); 
-//var socket = io.connect('localhost:8000'); 
-socket.on('news', function (data) { 
-    console.log(data.serverData);
-}); 
-
-// userid ??? db or websocket.id?
-socket.emit('client connected', 
-{ clientData : '드론 컨트롤러 접속', clientType : 'ctw', userid : userid}); 
-*/
-
 //module.exports = app;
